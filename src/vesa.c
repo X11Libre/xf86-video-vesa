7b2b0f3a15255176688a1e38008e1ce06b282a68
Enrico Weigelt, metux IT consult <info@metux.net>
2025-12-16 18:32:44 +0100

    drop support for ancient xextproto < 7.1
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/vesa.c
+++ src/vesa.c
@@ -57,12 +57,7 @@
 #include "xf86Modes.h"
 
 /* DPMS */
-#ifdef HAVE_XEXTPROTO_71
 #include <X11/extensions/dpmsconst.h>
-#else
-#define DPMS_SERVER
-#include <X11/extensions/dpms.h>
-#endif
 #include "compat-api.h"
 
 /* Mandatory functions */
